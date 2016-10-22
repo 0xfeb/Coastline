4abf7ebadabec69b84c48810616b5edb3e4153a9
王渊鸥 <wangyuanou@foxmail.com>
2016-10-22 16:39:29 +0800

    Initial Commit

---
+++ Coastline/Coastline.h
@@ -0,0 +1,19 @@
+//
+//  Coastline.h
+//  Coastline
+//
+//  Created by 王渊鸥 on 2016/10/22.
+//  Copyright © 2016年 王渊鸥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Coastline.
+FOUNDATION_EXPORT double CoastlineVersionNumber;
+
+//! Project version string for Coastline.
+FOUNDATION_EXPORT const unsigned char CoastlineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Coastline/PublicHeader.h>
+
+
