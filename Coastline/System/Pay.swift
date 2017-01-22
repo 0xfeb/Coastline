ce89f80c10b6587fc29c96c2b1d07c2740d67e56
王渊鸥 <wangyuanou@foxmail.com>
2017-01-22 18:00:32 +0800

    feat(CLPay): 添加苹果支付

---
+++ Coastline/System/Pay.swift
@@ -0,0 +1,53 @@
+//
+//  Pay.swift
+//  Coastline
+//
+//  Created by 王渊鸥 on 2017/1/22.
+//  Copyright © 2017年 王渊鸥. All rights reserved.
+//
+
+import StoreKit
+
+@objc public class CLPay : NSObject {
+	var payResult:(Bool)->Void = { _ in }
+	let sharedInstance = CLPay()
+	
+	override init() {
+		super.init()
+		
+		SKPaymentQueue.default().add(self)
+	}
+	
+	deinit {
+		SKPaymentQueue.default().remove(self)
+	}
+	
+	func fetchProduct(aid:String) {
+		let request = SKProductsRequest(productIdentifiers: Set(arrayLiteral: aid))
+		request.delegate = self
+		request.start()
+	}
+}
+
+extension CLPay : SKProductsRequestDelegate {
+	public func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+		if let product = response.products.first {
+			let payment = SKPayment(product: product)
+			SKPaymentQueue.default().add(payment)
+		} else {
+			print("找不到对应的商品信息")
+		}
+	}
+}
+
+extension CLPay : SKPaymentTransactionObserver {
+	public func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+		if let trans = transactions.first {
+			if trans.transactionState == .purchased {
+				payResult(true)
+			} else if trans.transactionState == .failed {
+				payResult(false)
+			}
+		}
+	}
+}
