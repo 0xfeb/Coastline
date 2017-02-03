e95ccf0ff53af4207c4d199c9597bc1e4ce64e3f
王渊鸥 <wangyuanou@foxmail.com>
2017-02-03 15:25:39 +0800

    fix (View): 添加动画效果

--- Coastline/UI/View+Animation.swift
+++ Coastline/UI/View+Animation.swift
@@ -32,4 +32,44 @@ public extension UIView {
 			}
 		}, completion: completion)
 	}
+	
+	public func popOut(_ duration:TimeInterval, completion:((Bool)-> Swift.Void)? = nil) {
+		guard let sv = self.superview else {
+			completion?(false)
+			return
+		}
+		
+		self.frameTop = sv.bounds.height
+		self.frameCenterH = sv.bounds.width / 2.0
+		
+		UIView.animateKeyframes(withDuration: duration+0.3, delay: 0, options: UIViewKeyframeAnimationOptions(), animations: { [weak self, weak sv, duration] in
+			guard let view = self, let sv = sv else { return }
+			
+			let p0 = duration / (duration + 0.3)
+			let pL = (1.0 - p0) / 3.0
+			UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: p0, animations: { [weak view, weak sv] in
+				guard let sv = sv else { return }
+				view?.frameTop = sv.bounds.height * 2.0 / 3.0
+			})
+			UIView.addKeyframe(withRelativeStartTime: p0, relativeDuration: pL, animations: {  [weak view, weak sv] in
+				guard let sv = sv else { return }
+				view?.frameTop = sv.bounds.height * 2.0 / 3.0 - 5.0
+			})
+			UIView.addKeyframe(withRelativeStartTime: p0+pL, relativeDuration: pL, animations: {  [weak view, weak sv] in
+				guard let sv = sv else { return }
+				view?.frameTop = sv.bounds.height * 2.0 / 3.0 + 5.0
+			})
+			UIView.addKeyframe(withRelativeStartTime: p0+2*pL, relativeDuration: pL, animations: {  [weak view, weak sv] in
+				guard let sv = sv else { return }
+				view?.frameTop = sv.bounds.height * 2.0 / 3.0
+			})
+		}, completion: completion)
+	}
+	
+	public func unPopOut(_ duration:TimeInterval, completion:((Bool)-> Swift.Void)? = nil) {
+		UIView.animate(withDuration: duration, animations: { [weak self] in
+			guard let sv = self?.superview else { return }
+			self?.frameTop = sv.bounds.height
+		}, completion: completion)
+	}
 }
