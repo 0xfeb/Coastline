9f5549bc591c1965e5cb0a8939032f421566cf26
王渊鸥 <wangyuanou@foxmail.com>
2017-02-06 12:14:44 +0800

    fix(URL): URL中加上可以产生链式调用的工具函数

--- Coastline/Structure/URL+Part.swift
+++ Coastline/Structure/URL+Part.swift
@@ -13,4 +13,33 @@ public extension URL {
 		guard let p = query else { return nil }
 		return p.components(separatedBy: "&").toDict{ $0.head2Parts(gap: "=") }
 	}
+	
+	public var paramStrings:[String]? {
+		guard let p = query else { return nil }
+		return p.components(separatedBy: "&")
+	}
+	
+	public func isScheme(scheme:String) -> URL? {
+		return self.scheme == scheme ? self : nil
+	}
+	
+	public func isHost(host:String) -> URL? {
+		return self.host == host ? self : nil
+	}
+	
+	public func isPath(path:String) -> URL? {
+		return self.path == path ? self : nil
+	}
+	
+	public func isPrePath(text:String) -> URL? {
+		return self.path.hasPrefix(text) ? self: nil
+	}
+	
+	public func isPostPath(text:String) -> URL? {
+		return self.path.hasSuffix(text) ? self: nil
+	}
+	
+	public func isParams(lines:[String]) -> URL? {
+		return  self.paramStrings?.includeAll(array: lines) == true ? self: nil
+	}
 }
