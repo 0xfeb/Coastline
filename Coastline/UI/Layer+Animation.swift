a0537471d7507f9d89f7186f614be893ee21cd4f
王渊鸥 <wangyuanou@foxmail.com>
2017-03-27 17:20:59 +0800

    UIView animation

---
+++ Coastline/UI/Layer+Animation.swift
@@ -0,0 +1,22 @@
+//
+//  Layer+Animation.swift
+//  Coastline
+//
+//  Created by 王渊鸥 on 2017/3/27.
+//  Copyright © 2017年 王渊鸥. All rights reserved.
+//
+
+import UIKit
+
+public extension CALayer {
+	public func addCycleAnimation() {
+		let ani = CABasicAnimation(keyPath: "transform.rotation.z")
+		ani.toValue = NSNumber(value: M_PI * 15)
+		ani.duration = 8
+		ani.repeatCount = 20
+		self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+		self.add(ani, forKey: "rotation")
+	}
+}
+
+
--- Coastline/UI/View+Animation.swift
+++ Coastline/UI/View+Animation.swift
@@ -72,4 +72,94 @@ public extension UIView {
 			self?.frameTop = sv.bounds.height
 		}, completion: completion)
 	}
+	
+	
+	
+	
+	func startWaiting(title:String, timeout:TimeInterval = 5.0) {
+		killWaiting()
+		
+		let backView = UIView(frame: self.bounds)
+		backView.tag = 12301
+		backView.backgroundColor = UIColor(colorLiteralRed: 0.2, green: 0.2, blue: 0.2, alpha: 0.4)
+		self.addSubview(backView)
+		
+		let barView = UIView(frame: CGRect(x: 0, y: 0, width: 120, height: 120))
+		barView.backgroundColor = UIColor(colorLiteralRed: 0.4, green: 0.4, blue: 0.4, alpha: 0.9)
+		barView.frameCetner = backView.boundsCenter
+		barView.layer.masksToBounds = true
+		barView.layer.cornerRadius = 8.0
+		backView.addSubview(barView)
+		
+		let pgLayer = CAShapeLayer()
+		let path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 40, height: 40))
+		path.lineWidth = 20
+		pgLayer.fillColor = nil
+		pgLayer.strokeColor = UIColor.white.cgColor
+		pgLayer.path = path.cgPath
+		pgLayer.strokeStart = 0.0
+		pgLayer.strokeEnd = 0.8
+		pgLayer.lineWidth = 5
+		pgLayer.lineCap = kCALineCapRound
+		pgLayer.frame = CGRect(center:barView.bounds.center, size:CGSize(width:40, height:40)).offsetBy(dx: 0, dy: -5)
+		barView.layer.addSublayer(pgLayer)
+		pgLayer.addCycleAnimation()
+		
+		let label = UILabel(frame: CGRect(x: 0, y: 0, width: 120, height: 20))
+		label.font = UIFont.systemFont(ofSize: 14)
+		label.textColor = UIColor.white
+		label.text = title
+		label.frameCetner = barView.boundsCenter
+		label.textAlignment = .center
+		label.frameTop += 40
+		barView.addSubview(label)
+		
+		if timeout > 0.0 {
+			Timer.scheduledTimer(timeInterval: timeout, target: self, selector: #selector(killWaiting), userInfo: nil, repeats: false)
+		}
+	}
+	
+	func killWaiting() {
+		let view = self.viewWithTag(12301)
+		view?.removeFromSuperview()
+	}
+	
+	func startHud(title:String, timeout:TimeInterval = 0.0) {
+		killHud()
+		
+		let textSize = title.textSize(UIFont.systemFont(ofSize: 14))
+		
+		let barView = UIView(frame: CGRect(x: 0, y: 0, width: textSize.width + 20, height: textSize.height + 10))
+		barView.tag = 12302
+		barView.backgroundColor = UIColor.black
+		barView.layer.masksToBounds = true
+		barView.layer.cornerRadius = 8.0
+		self.addSubview(barView)
+		
+		let label = UILabel(frame: barView.bounds)
+		label.font = UIFont.systemFont(ofSize: 14)
+		label.textColor = UIColor.white
+		label.text = title
+		label.textAlignment = .center
+		barView.addSubview(label)
+		
+		barView.popOut(0.1) { [weak self, timeout] (r) in
+			guard let s = self else { return }
+			if r && timeout > 0.0 {
+				Timer.scheduledTimer(timeInterval: timeout, target: s, selector: #selector(s.killHud), userInfo: nil, repeats: false)
+			}
+		}
+	}
+	
+	func stopHud() {
+		let view = self.viewWithTag(12302)
+		view?.unPopOut(0.2, completion: { [weak self] (r) in
+			self?.killHud()
+		})
+	}
+	
+	func killHud() {
+		let view = self.viewWithTag(12302)
+		view?.removeFromSuperview()
+	}
 }
