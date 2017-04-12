aab8cee39c91f528ed54b41c7ce200f5fc2909df
王渊鸥 <wangyuanou@foxmail.com>
2017-04-12 12:21:33 +0800

    fix(CycleView)

--- Coastline/UI/CLCycleView.swift
+++ Coastline/UI/CLCycleView.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class CLCycleView: UIView {
-	public var colors:[UIColor] = [UIColor(colorLiteralRed: 1, green: 0, blue: 1, alpha: 0.4),
-	                               UIColor(colorLiteralRed: 0, green: 1, blue: 1, alpha: 0.4),
-	                               UIColor(colorLiteralRed: 1, green: 1, blue: 0, alpha: 0.4)]
-
+public class CLCycleView: UIView {
+	public var colors:[UIColor] = [UIColor(colorLiteralRed: 1, green: 0, blue: 1, alpha: 0.9),
+	                               UIColor(colorLiteralRed: 0, green: 1, blue: 1, alpha: 0.9),
+	                               UIColor(colorLiteralRed: 1, green: 1, blue: 0, alpha: 0.9)]
+	
+	var lineWidth:CGFloat = 20.0
 	var currentRate:CGFloat = 0
 	var timer:Timer?
 	public var scores:[Int] = [0, 0, 0] {
 		didSet {
-			timer = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector(scoreTimerAction), userInfo: nil, repeats: true)
+			timer = Timer.scheduledTimer(timeInterval: 0.03, target: self, selector: #selector(scoreTimerAction), userInfo: nil, repeats: true)
 		}
 	}
 	
@@ -48,17 +49,15 @@ class CLCycleView: UIView {
 	}
 	
 	func pathOfScore(item:Int, offset:Int, rect:CGRect, step:Int) -> UIBezierPath? {
-		let prc = rect.bigger(rate: CGFloat(offset)/3.0 )
+		let prc = rect.bigger(rate: (CGFloat(offset) + 1.0)/5.0 )
 		
 		let score = CGFloat(item) * currentRate
 		
-		let path = UIBezierPath()
-		_ = path.move(prc.midX, prc.minX)
-		path.addArc(withCenter: prc.center, radius: prc.width/2.0, startAngle: CGFloat.pi * 3.0 / 4.0, endAngle: CGFloat.pi * score * 2.0 / CGFloat(step) + CGFloat.pi / 2.0, clockwise: true)
+		let path = UIBezierPath(arcCenter: prc.center, radius: prc.width/2.0, startAngle: CGFloat.pi * 3.0 / 2.0, endAngle: CGFloat.pi * score * 2.0 / CGFloat(step) + CGFloat.pi / 2.0, clockwise: true)
 		
 		path.lineCapStyle = .round
-		path.lineWidth = 10.0
-	
+		path.lineWidth = lineWidth
+		
 		return path
 	}
 }
