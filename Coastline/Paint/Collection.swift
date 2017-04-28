75e4921bef15a99fcb434906b81c1ef8d1bd008c
王渊鸥 <wangyuanou@foxmail.com>
2017-04-28 17:14:27 +0800

    fix(Collection)

--- Coastline/Paint/Collection.swift
+++ Coastline/Paint/Collection.swift
@@ -21,7 +21,7 @@ public struct CLCollection {
 	}
 	
 	public func vectorIndexOfIndex(_ index:Int) -> (Int, Int) {
-		return (index / itemsInRow, index & itemsInRow)
+		return (index / itemsInRow, index % itemsInRow)
 	}
 	
 	public func itemSize() -> CGSize {
@@ -34,15 +34,15 @@ public struct CLCollection {
 	public func rectForItem(_ index:Int) -> CGRect {
 		let vIndex = vectorIndexOfIndex(index)
 		let nSize = itemSize()
-		return CGRect(x: insets.left + CGFloat(vIndex.1) * nSize.width,
-		              y: insets.top + CGFloat(vIndex.0) * nSize.height,
+		return CGRect(x: insets.left + CGFloat(vIndex.1) * nSize.width + CGFloat(vIndex.1) * gaps.width,
+		              y: insets.top + CGFloat(vIndex.0) * nSize.height + CGFloat(vIndex.0) * gaps.height,
 		              width: nSize.width, height: nSize.height)
 	}
 	
 	public func totalHeight() -> CGFloat {
 		let lastRow = (number - 1)  / itemsInRow
 		let nSize = itemSize()
-		return insets.top + insets.bottom + CGFloat(lastRow) * nSize.height
+		return insets.top + insets.bottom + CGFloat(lastRow) * nSize.height + CGFloat(lastRow + 1) * gaps.height
 	}
 	
 	public static func setCellSizeForCollectionView(_ collectionView:UICollectionView, itemsInRow:Int, itemWidthHeightRate:CGFloat, totalWidth:CGFloat) -> CGSize {
@@ -56,5 +56,17 @@ public struct CLCollection {
 	public static func buildTabList(edge:CGFloat, gap:CGFloat, itemsInRow:Int, itemWidthHeightRate:CGFloat, totalWidth:CGFloat) -> [CGRect] {
 		let sizeCollection = CLCollection(insets: UIEdgeInsets(top: 0, left: edge, bottom: 0, right: edge), gaps: CGSize(width: gap, height: 0), itemsInRow: itemsInRow, number: itemsInRow, totalWidth: totalWidth, heightWidthRate: itemWidthHeightRate)
 		return (0..<itemsInRow).map { sizeCollection.rectForItem($0) }
+	}
+}
+
+public extension UIView {
+	public func addCollection(views:[UIView], insets:UIEdgeInsets, gaps:CGSize, itemsInRow:Int, hwRate:CGFloat) -> CGFloat {
+		let col = CLCollection(insets: insets, gaps: gaps, itemsInRow: itemsInRow, number: views.count, totalWidth: self.bounds.width, heightWidthRate: hwRate)
+		views.enumerated().forEach{
+			$0.element.frame = col.rectForItem($0.offset)
+			print($0.element.frame)
+			self.addSubview($0.element)
+		}
+		return col.totalHeight()
 	}
 }
