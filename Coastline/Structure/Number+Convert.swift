a4aa7dd35e515d93b5e9fbaa0f91192585608dd9
王渊鸥 <wangyuanou@foxmail.com>
2017-05-16 14:32:48 +0800

    number limit

--- Coastline/Structure/Number+Convert.swift
+++ Coastline/Structure/Number+Convert.swift
@@ -12,10 +12,17 @@ public extension Double {
 	public var intValue:Int {
 		return (String(format:"%.0f", self) as NSString).integerValue
 	}
+	
 }
 
 public extension Float {
 	public var intValue:Int {
 		return (String(format:"%.0f", self) as NSString).integerValue
 	}
+}
+
+public func limit<T:Comparable>(_ value:T, start:T, end:T) -> T {
+	if value < start { return start }
+	if value > end { return end }
+	return value
 }
